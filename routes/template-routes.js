49a8dc4646b98b1e17c3c96bb01775e0ac35440f
burntsugar <rrakky89@gmail.com>
2020-01-23 10:55:34 +1100

    add entries for ass endpoints

--- routes/template-routes.js
+++ routes/template-routes.js
@@ -3,16 +3,29 @@ const templateRouter = express.Router();
 
 import {templateController} from '../controllers/template-controller.js';
 
+templateRouter.use((req, res, next)=> {
+  console.log(req.hostname + req.baseUrl + req.url);
+  next();
+});
+
 templateRouter.get('/', (req, res) => {
-  console.log('/');
+  // Base view not implemented.
+  res.status(404).send('Not Found - Not Implemented ~ rach@rach');
+});
+
+templateRouter.get('/aboutus', (req, res) => {
   const data = templateController.makeTemplateFragment('ABOUT US');
   res.render('about', data);
 });
 
 templateRouter.get('/contactus', (req, res) => {
-  console.log('/contactus');
   const data = templateController.makeTemplateFragmentForContactUs();
   res.render('contactus', data);
+});
+
+templateRouter.get('/contactus-response', (req, res) => {
+  const data = templateController.makeTemplateFragmentForContactUs();
+  res.render('contactus-response', data);
 });
 
 export {templateRouter};
