05c713cf1c68fdccb1d444574326296846c6d207
burntsugar <rrakky89@gmail.com>
2020-01-22 14:29:42 +1100

    linting...

--- keys.js
+++ keys.js
@@ -1,6 +1,12 @@
+/**
+ * @file
+ * @author Rachael Colley <rcolley@rcolley>
+ * @fileoverview Provides access to environment variables.
+ */
+
 const envKeys = {
-    PORT: process.env.PORT || 3000,
-    GMAPSAPIKEY: process.env.GMAPKEY || 'not set',  
+  PORT: process.env.PORT || 3000,
+  GMAPSAPIKEY: process.env.GMAPKEY || 'not set',
 };
 
 export {envKeys};
