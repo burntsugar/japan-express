594bc4bd4cb2ca61f5afb1d48cf736310687d390
burntsugar <rrakky89@gmail.com>
2020-01-23 14:31:26 +1100

    refactor

--- public/scripts/prefillcity.js
+++ public/scripts/prefillcity.js
@@ -1,18 +1,17 @@
 $(document).ready(function() {
   'use strict';
 
-  const urlParams = new URLSearchParams(location.search);
-
-  if (urlParams.has('tour')) {
-    const s = urlParams.get('tour');
-    $('#packageselect').val(s).change();
+  const tourName = $('#hidden-tour').text();
+  console.log('tourname: ' + tourName);
+  if (tourName.length > 0) {
+    $('#packageselect').val(tourName).change();
     $('#buttonAlert').addClass('show');
     $('#packageselect').trigger('focus');
-    $('#selectedqcity').text('You have selected ' + s + '!');
-
-
-    $('#buttonAlert').on('closed.bs.alert', function() {
-      $('#fname').focus();
-    });
+    $('#selectedqcity').text('You have selected ' + tourName + '!');
   }
+
+  $('#buttonAlert').on('closed.bs.alert', function() {
+    $('#fname').focus();
+  });
 });
+
