fbac253ce6d38265b7ebdd930a0c0ba800034141
burntsugar <rrakky89@gmail.com>
2020-01-23 16:20:25 +1100

    added supertest suite for pages-controller

--- test/pages-controller-spec.js
+++ test/pages-controller-spec.js
@@ -1,18 +1,109 @@
-import {pageRoutes} from '../routes/page-routes.js';
+import {app} from '../index.js';
 import Test from 'supertest';
+import Cheerio from 'cheerio';
 
-describe('GET /', () => {
+describe('GET /pages/', () => {
   let myapp = null;
 
   beforeEach(() => {
-    myapp = pageRoutes;
+    myapp = app;
   });
 
-  it('responds with text/html; charset=UTF-8', (done) => {
-    console.log(done);
+  it('responds with 404', (done) => {
     Test(myapp)
-      .get('/')
+      .get('/pages/')
+      .expect(404, done);
+  });
+
+  it('responds with text/html; charset=utf-8', (done) => {
+    Test(myapp)
+      .get('/pages/aboutus')
       .expect('Content-Type', 'text/html; charset=utf-8')
       .expect(200, done);
+  });
+
+  it('document title is About us', (done) => {
+    Test(myapp)
+      .get('/pages/aboutus')
+      .expect((res) => {
+        const htmlResponse = res.text;
+        const chata = Cheerio.load(htmlResponse);
+        const actual = chata('title').text();
+        const expected = 'About us';
+        if (actual != expected) {
+          throw new Error(expected, (actual) => {
+            `Expected ${expected} but got ${actual}!`;
+          });
+        }
+      }).end(done);
+  });
+
+  it('responds with text/html; charset=utf-8', (done) => {
+    Test(myapp)
+      .get('/pages/contactus')
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect(200, done);
+  });
+
+  it('document title is Contact us', (done) => {
+    Test(myapp)
+      .get('/pages/contactus')
+      .expect((res) => {
+        const htmlResponse = res.text;
+        const chata = Cheerio.load(htmlResponse);
+        const actual = chata('title').text();
+        const expected = 'Contact us';
+        if (actual != expected) {
+          throw new Error(expected, (actual) => {
+            `Expected ${expected} but got ${actual}!`;
+          });
+        }
+      }).end(done);
+  });
+
+  it('responds with text/html; charset=utf-8', (done) => {
+    Test(myapp)
+      .get('/pages/contactus-response')
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect(200, done);
+  });
+
+  it('document title is Contact us response', (done) => {
+    Test(myapp)
+      .get('/pages/contactus-response')
+      .expect((res) => {
+        const htmlResponse = res.text;
+        const chata = Cheerio.load(htmlResponse);
+        const actual = chata('title').text();
+        const expected = 'Contact us response';
+        if (actual != expected) {
+          throw new Error(expected, (actual) => {
+            `Expected ${expected} but got ${actual}!`;
+          });
+        }
+      }).end(done);
+  });
+
+  it('responds with text/html; charset=utf-8', (done) => {
+    Test(myapp)
+      .get('/pages/planyourtrip')
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect(200, done);
+  });
+
+  it('document title is Plan your trip', (done) => {
+    Test(myapp)
+      .get('/pages/planyourtrip')
+      .expect((res) => {
+        const htmlResponse = res.text;
+        const chata = Cheerio.load(htmlResponse);
+        const actual = chata('title').text();
+        const expected = 'Plan your trip';
+        if (actual != expected) {
+          throw new Error(expected, (actual) => {
+            `Expected ${expected} but got ${actual}!`;
+          });
+        }
+      }).end(done);
   });
 });
